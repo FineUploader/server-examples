f43a0c4d605bb29d5d3cb97f9d3a02f9f0086136
Jeremy Dickens <jeremy@7sudos.com>
2013-04-16 11:27:30 -0500

    Basic example code for node.js / express implementation

--- nodejs.js
+++ nodejs.js
@@ -5,6 +5,9 @@
  * npm install express
  * npm install node-uuid
  *
+ * Express handles most of the heavy lifting of handling the multipart form parsing - all we have to do is establish an endpoint
+ * to handle the incoming file
+ *
  * If you are using NginX as reverse proxy, please set this in your server block:
  * client_max_body_size    200M;
  * 
@@ -26,96 +29,33 @@ var express = require('express'),
 // Settings
 var settings = {
     node_port: process.argv[2] || 3000,
-    uploadpath: __dirname + '/uploads/'
+    uploadPath: __dirname + '/uploads/'
 };
 
-// Configuration
-app.configure(function(){
-    // We need the bodyParser to form parsing old style uploads
-    app.use(express.bodyParser());
+app.use(express.bodyParser({uploadDir: settings.uploadPath}));
 
-    app.use(express.methodOverride());
-    app.use(app.router);
-});
+app.post('/upload', function(request, response, next {
+    // as the uploadDir is simply a temporary save location, let's define a permanant place for 
+    // uploads to be saved
 
-// Route that takes the post upload request and sends the server response
-app.post('/upload', function(req, res) {
-    uploadFile(req, settings.uploadpath, function(data) {
-        if(data.success)
-            res.send(JSON.stringify(data), {'Content-Type': 'text/plain'}, 200);
-        else
-            res.send(JSON.stringify(data), {'Content-Type': 'text/plain'}, 404);
-    });
-});
+    var savePath = __dirname + '/files/';
+    var fileName = request.files.qqfile.name;
 
-
-// Mainfunction to recieve and process the file upload data asynchronously
-var uploadFile = function(req, targetdir, callback) {
-
-    // Moves the uploaded file from temp directory to it's destination
-    // and calls the callback with the JSON-data that could be returned.
-    var moveToDestination = function(sourcefile, targetfile) {
-        moveFile(sourcefile, targetfile, function(err) {
-            if(!err)
-                callback({success: true});
-            else
-                callback({success: false, error: err});
-        });
-    };
-
-    // Direct async xhr stream data upload, yeah baby.
-    if(req.xhr) {
-        var fname = url.parse(req.url, true).query.qqfile;
-
-        // Be sure you can write to '/tmp/'
-        var tmpfile = '/tmp/'+uuid.v1();
-
-        // Open a temporary writestream
-        var ws = fs.createWriteStream(tmpfile);
-        ws.on('error', function(err) {
-            console.log("uploadFile() - req.xhr - could not open writestream.");
-            callback({success: false, error: "Sorry, could not open writestream."});
-        });
-        ws.on('close', function(err) {
-            moveToDestination(tmpfile, targetdir+fname);
-        });
-
-        // Writing filedata into writestream
-        req.on('data', function(data) {
-            ws.write(data);
-        });
-        req.on('end', function() {
-            ws.end();
-        });
-    }
-
-    // Old form-based upload
-    else {
-        moveToDestination(req.files.qqfile.path, targetdir+req.files.qqfile.name);
-    }
-};
-
-// Moves a file asynchronously over partition borders
-var moveFile = function(source, dest, callback) {
-    var is = fs.createReadStream(source)
-
-    is.on('error', function(err) {
-        console.log('moveFile() - Could not open readstream.');
-        callback('Sorry, could not open readstream.')
-    });
-    is.on('end', function() {
-        fs.unlinkSync(source);
-        callback();
-    });
+    //after upload, rename the file and respond to Fine Uploader to notify it of success
+    fs.rename(request.files.qqfile.path, savePath + fileName, function(err) {
+	if err(!=null) {
+	    console.log('Err: ' + err);
+	    response.send(JSON.stringify({success: false, error: err}), {'Content-Type': 'text/plain'}, 404);
+	} 
+	else {
+	    response.send(JSON.stringify({success: true}), {'Content-Type': 'text/plain'}, 200);
+	    console.log('File Uploaded: ' + savePath + fileName);
+	}
+    })
     
-    var os = fs.createWriteStream(dest);
-    os.on('error', function(err) {
-        console.log('moveFile() - Could not open writestream.');
-        callback('Sorry, could not open writestream.');
-    });
-    
-    is.pipe(os);
-};
+}));
+
+
 
 // Starting the express server
 app.listen(settings.node_port, '127.0.0.1');
