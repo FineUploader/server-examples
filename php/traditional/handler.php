aa7838dbffcf2a119a3226c029bfff906da8c83c
Thomas Tourlourat <thomas@tourlourat.com>
2013-09-25 14:09:44 +0200

    Guess it will work better

--- php/traditional/handler.php
+++ php/traditional/handler.php
@@ -271,8 +271,8 @@ class UploadHandler {
         $val = trim($str);
         $last = strtolower($str[strlen($str)-1]);
         switch($last) {
-            case 'g': $val *= 1024;
-            case 'm': $val *= 1024;
+            case 'g': $val *= 1024 * 1024 * 1024;
+            case 'm': $val *= 1024 * 1024;
             case 'k': $val *= 1024;
         }
         return $val;
