ffccb9d2e0fd27543656250da30ae30e20612bfe
Mark Feltner <mfeltner@widen.com>
2014-01-02 11:13:24 -0600

    chore($python-trad): No client-side code

--- python/django-fine-uploader/static/main.js
+++ python/django-fine-uploader/static/main.js
@@ -1,22 +1,5 @@
 $(document).ready(function() {
 
-    $("#fine-uploader").fineUploader({
-        request: {
-            endpoint: '/upload'
-        },
-        chunking: {
-            enabled: true
-        },
-        resume: {
-            enabled: true
-        },
-        retry: {
-            enableAuto: true
-        },
-        deleteFile: {
-            enabled: true,
-            endpoint: '/upload'
-        }
-    });
+    /** instantiate Fine Uploader here */
 
 });
