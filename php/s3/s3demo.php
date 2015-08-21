bc670ceff0c7ccfd15c0ea976d4bcf0a83b7373c
Ray Nicholus <rnicholus@users.noreply.github.com>
2015-08-21 13:12:00 -0500

    typo

--- php/s3/s3demo.php
+++ php/s3/s3demo.php
@@ -1,2 +1,2 @@
 The S3 PHP server example has moved to a new repository: https://github.com/FineUploader/php-s3-server.
-The server code is also available on packagist via composer under the name `fineuploader/php-s3-server`.
+The server code is also available on packagist via composer under the name fineuploader/php-s3-server.
